99580efd10db88c8f9a3c7fb70453eb2acf402d4
Scott Sewards <scott.sewards@outlook.com>
2021-08-25 02:18:19 +0100

    Update 1.1 Containers and Breakpoints
    Removed spacing.
    Removed various margins.
    Corrected style errors on legend, dialog, and inputs.
    Changed kbd style.
    Added container and container-fill.
    Removed row-fix.
    Fixed row margin.
    Corrected breakpoints.
    Renamed color classes to text.
    Added more text classes.
    
    Condensed variables.css from 105 lines to 93.
    Condensed layout.css from 72 lines to 58.
    Condensed content.css from 420 lines to 301.
    Condensed classes.css from 530 lines to 517.

--- js/ss-functions.js
+++ js/ss-functions.js
@@ -1,52 +1,20 @@
-/* jQuery INSPIRED SELECTORS */
-const $ = document.querySelector.bind(document);
-const $$ = (css, parent = document) => Array.from(parent.querySelectorAll(css));
 
-/* RUN CODE ON DOCUMENT READY */
+var addDropDownButtons = () => document.querySelectorAll(".drop-down-button").forEach((button) => button.addEventListener("click", () => button.parentElement.classList.toggle("active")));
+var setImageIntrinsicWidth = () => document.querySelectorAll("img").forEach((img) => img.setAttribute("style", `max-width: ${img.naturalWidth}px`));
 
-function initScottsFunctions() {
-  /* ENABLE drop-list-button TO HIDE OR UNHIDE drop-list-* */
-  $$(".drop-down-button").forEach((element) => {
-    element.addEventListener("click", function() {
-      this.parentElement.classList.toggle("active");
-    });
-  });
-
-  /* APPLY img INTRINSIC WIDTH AS max-width */
-  $$("img").forEach((img, index) => img.setAttribute("style", `max-width: ${img.naturalWidth}px`));
-
-  $$("[type='input']").forEach((element) => {
-    element.attr("aria-label", `${element.name}`);
-  });
-}
-
-/* COOKIE FUNCTIONS */
-var getCookie = (name) => {
+// COOKIE FUNCTIONS
+var getCookie = (name, element = 1) => {
   const cookie = document.cookie.split("; ").find(row => row.startsWith(`${name}=`));
-  return cookie !== undefined ? cookie.split("=")[1] : null;
+  return cookie !== undefined ? cookie.split("=")[element] : null;
 }
-var getCookieValue = (value) => {
-  const cookie = document.cookie.split("; ").find(row => row.startsWith(`${name}=`));
-  return cookie !== undefined ? cookie.split("=")[0] : null;
-}
-
+var getCookieValue = (value) => { return getCookie(value, 0); }
 var setCookie = (name, value) => document.cookie = `${name}=${value}`;
 var issetCookie = (name) => getCookie(name) !== null ? true : false;
 var issetCookieValue = (value) => getCookieValue(value) !== null ? true : false;
-
-var printCookie = (name) => console.log(document.cookie.split("; ").find(row => row.startsWith(`${name}=`)) !== null ? `${name}=` + getCookie(name) : "Cookie does not exist.");
-var printCookies = () => console.table(document.cookie);
-
+var printCookie = (name) => log(document.cookie.split("; ").find(row => row.startsWith(`${name}=`)) !== null ? `${name}=` + getCookie(name) : "Cookie does not exist.");
+var printCookies = () => table(document.cookie);
 var unsetCookie = (name) => document.cookie = issetCookie(name) === true ? `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT` : null;
-//var unsetCookies = () => console.log("");
+//var unsetCookies = () => log("");
 
-
-/* CLASS FUNCTIONS */
-function transitionElements(transitionTime = "0.1s") {
-  $$("[class*='transition-']").forEach((element, i) => {
-    if(element.classList.contains("transition-all")) element.style.setProperty("transition", `* ${transitionTime}`);
-    else {
-      element.style.setProperty("transition", `background 1s`);
-    }
-  });
-}
+var log = (value) => console.log(value);
+var table = (value) => console.table(value);
