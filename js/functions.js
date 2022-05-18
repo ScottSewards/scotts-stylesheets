7e08d33d4d78311cd945ac96f6152d4a6d172e98
Scott Sewards <scott.sewards@outlook.com>
2022-05-18 23:37:43 +0100

    1.3.0
    Updated .container max-width to use variable.
    Added bg- and text-error classes.
    Added text-transform classes.
    Removed -3 and -4 suffixed classes for padding, margin, and gap.
    Corrected .floating-label, caption, and figcaption color.
    Added multiple select styling.
    Removed figure img rules.
    Reformatted custom.css.
    Moved navigation drop down classes to layout.css.
    Moved prefers-reduced media queries to custom.css.
    Added console.warn shorthand function.
    Added getCookies function.
    Removed getCookie function second parameter.
    Changed getCookie function to return full cookie as string.
    Added validation to getCookieValue function.
    Removed issetCookieValue function.
    Renamed print cookie functions to logCookie and logCookies.
    Disabled unset cookie functions.
    Added setAllImgIntrinsicWidth function.
    Renamed addDropDownButtons function to setDropDowns.
    Fixed theme colour hue and dark mode functionality.

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,21 @@
-//PRINT FUNCS
-var log = (value) => console.log(value);
-var table = (value) => console.table(value);
-// COOKIE FUNCS
-var getCookie = (name, element = 1) => {
-  const cookie = document.cookie.split("; ").find(row => row.startsWith(`${name}=`));
-  return cookie !== undefined ? cookie.split("=")[element] : null;
-}
-var getCookieValue = (value) => { return getCookie(value, 0); }
-var setCookie = (name, value) => document.cookie = `${name}=${value}`;
-var issetCookie = (name) => getCookie(name) !== null ? true : false;
-var issetCookieValue = (value) => getCookieValue(value) !== null ? true : false;
-var printCookie = (name) => log(document.cookie.split("; ").find(row => row.startsWith(`${name}=`)) !== null ? `${name}=` + getCookie(name) : "Cookie does not exist.");
-var printCookies = () => table(document.cookie);
-var unsetCookie = (name) => document.cookie = issetCookie(name) === true ? `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT` : null;
-//var unsetCookies = () => log("");
-//EXTRA FUNCS
-var addDropDownButtons = () => document.querySelectorAll(".drop-down-button").forEach((button) => button.addEventListener("click", () => button.parentElement.classList.toggle("active")));
-var setImageIntrinsicWidth = () => document.querySelectorAll("img").forEach((img) => img.setAttribute("style", `max-width: ${img.naturalWidth}px`));
+var setDropDowns = () => document.querySelectorAll(".drop-down-button").forEach((b) => b.addEventListener("click", () => b.parentElement.classList.toggle("active"))),
+    setImgIntrinsicWidth = (e) => document.querySelector(e).setAttribute("style", `max-width: ${e.naturalWidth}px`),
+    setAllImgIntrinsicWidth = () => document.querySelectorAll("img").forEach((e) => el.setAttribute("style", `max-width: ${e.naturalWidth}px`));
+//CONSOLE
+var log = (v) => console.log(v), table = (v) => console.table(v), warn = (v) => console.warn(v);
+//COOKIE
+var defaultCookie = (n, v) => { if(getCookie(n) == null) document.cookie = `${n}=${v}` },
+    getCookies = () => { return document.cookie },
+    logCookies = () => log(getCookies()),
+    getCookie = (n) => {
+      var c = document.cookie.split("; ").find(r => r.startsWith(`${n}=`));
+      return c !== undefined ? c : null;
+    },
+    getCookieSplit = (n) => { return getCookie(n).split("=") },
+    setCookie = (n, v) => document.cookie = `${n}=${v}`,
+    issetCookie = (n) => getCookie(n) != null ? true : false,
+    logCookie = (n) => log(getCookie(n)),
+    getCookieValue = (n) => { return getCookie(n) != null ? getCookie(n).split("=")[1] : null },
+    logCookieValue = (n) => log(getCookieValue(n));
+    //unsetCookie = (n) => document.cookie = issetCookie(n) === true ? `${n}=; expires=Thu, 01 Jan 1970 00:00:00 GMT` : null,
+    //unsetCookies = () => document.cookie = "";
