e640d92fddb4f7cb545ad74d2f4dc039cd5b69bf
Scott Sewards <scott.sewards@outlook.com>
2022-07-08 12:31:15 +0100

    1.4.2 Column Gap Variable and Default Cookies Function Update
    Added --col-gap variable to set column margins.
    Removed code tab-size property.
    Added error shorthand function.
    Added defaultCookies function.
    Updated content.

--- js/functions.js
+++ js/functions.js
@@ -2,9 +2,10 @@ var setDropDowns = () => document.querySelectorAll(".drop-down-button").forEach(
     setImgIntrinsicWidth = (e) => document.querySelector(e).setAttribute("style", `max-width: ${e.naturalWidth}px`),
     setAllImgIntrinsicWidth = () => document.querySelectorAll("img").forEach((e) => el.setAttribute("style", `max-width: ${e.naturalWidth}px`));
 //CONSOLE
-var log = (v) => console.log(v), table = (v) => console.table(v), warn = (v) => console.warn(v);
+var log = (v) => console.log(v), table = (v) => console.table(v), warn = (v) => console.warn(v), error = (v) => console.error(v);
 //COOKIE
-var defaultCookie = (n, v) => { if(getCookie(n) == null) document.cookie = `${n}=${v}` },
+var defaultCookies = (a) => Object.entries(a).forEach((e) => setCookie(e[0], e[1])),
+    defaultCookie = (n, v) => { if(getCookie(n) == null) document.cookie = `${n}=${v}` },
     getCookies = () => { return document.cookie },
     getCookiesSplit = () => { return document.cookie.split(";") },
     logCookies = () => log(document.cookie),
@@ -21,5 +22,4 @@ var defaultCookie = (n, v) => { if(getCookie(n) == null) document.cookie = `${n}
     //unsetCookie = (n) => document.cookie = issetCookie(n) === true ? `${n}=; expires=Thu, 01 Jan 1970 00:00:00 GMT` : null,
     //unsetCookies = () => document.cookie = "";
 
-
-function roPreventDefault() {}
+//function roPreventDefault() {}
